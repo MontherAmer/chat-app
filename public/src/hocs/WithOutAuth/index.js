4dc1cbbaa7b773c1a588237db0354ed6d560a810
montheramer <monther.amer48@gmail.com>
2020-11-02 18:55:12 +0200

    ADD hocs for auth and not auth screens

--- public/src/hocs/WithOutAuth/index.js
+++ public/src/hocs/WithOutAuth/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-function withAuth(Wrapped) {
+function withOutAuth(Wrapped) {
   return function (props) {
     const { userState } = useSelector(state => state);
-    if (!userState._id) useHistory().push('/login');
+    if (userState._id) useHistory().push('/');
     return <Wrapped {...props} />;
   };
 }
 
-export default withAuth;
+export default withOutAuth;
