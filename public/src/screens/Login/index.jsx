f9ea8f335c7ab7c17d0de32b954ead792b3c2287
montheramer <monther.amer48@gmail.com>
2020-11-02 22:08:28 +0200

    CREATE toggle input and add it to auth screens

--- public/src/screens/Login/index.jsx
+++ public/src/screens/Login/index.jsx
@@ -3,13 +3,17 @@ import { useDispatch } from 'react-redux';
 
 import { login } from '../../store/actions';
 
+import ToggleButton from '../../components/ToggleButton';
+
 export default () => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({ rememberMe: false });
   const dispatch = useDispatch();
 
   const handleChange = e => setState({ ...state, [e.target.name]: e.target.value });
 
   const handleClick = () => dispatch(login(state));
+
+  const handleToggle = e => setState({ ...state, rememberMe: !state.rememberMe });
 
   return (
     <div className='auth'>
@@ -17,6 +21,7 @@ export default () => {
         <h1 className='title3_txt c_gray'>Log In</h1>
         <input autoComplete='off' className='auth--input' placeholder='Email' type='text' name='email' onChange={handleChange} />
         <input autoComplete='off' className='auth--input' placeholder='Password' type='password' name='password' onChange={handleChange} />
+        <ToggleButton checked={state.rememberMe} handleClick={handleToggle} label='Remeber me' />
         <button className='auth--button' onClick={handleClick}>
           Log In
         </button>
--- public/src/screens/SignUp/index.jsx
+++ public/src/screens/SignUp/index.jsx
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { signUp } from '../../store/actions';
+import ToggleButton from '../../components/ToggleButton';
 
 export default () => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({ rememberMe: false });
   const dispatch = useDispatch();
 
   const handleChange = e => setState({ ...state, [e.target.name]: e.target.value });
 
-  const handleClick = () => dispatch(signUp(state));
+  const handleToggle = e => setState({ ...state, rememberMe: !state.rememberMe });
 
+  const handleClick = () => dispatch(signUp(state));
   return (
     <div className='auth'>
       <div className='auth-container'>
@@ -25,6 +27,7 @@ export default () => {
           name='password2'
           onChange={handleChange}
         />
+        <ToggleButton checked={state.rememberMe} handleClick={handleToggle} label='Remeber me' />
         <button className='auth--button' onClick={handleClick}>
           Sign Up
         </button>
