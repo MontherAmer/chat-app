ada71ac7f1f1d387d2443fbedace69256a6d34b9
montheramer <monther.amer48@gmail.com>
2020-11-02 17:54:09 +0200

    wrap components in provider

--- public/src/index.js
+++ public/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import configureStore from './store';
+
+import fireApi from './store/apis';
+
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+fireApi();
+
+export const { store, persistor } = configureStore();
+
+const render = Component => {
+  return ReactDOM.render(
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Component />
+      </PersistGate>
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
