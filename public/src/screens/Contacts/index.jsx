1a1ef4b1e6d4e85a9c283a3637a7e6ca4fcc7c31
montheramer <monther.amer48@gmail.com>
2020-11-04 12:08:12 +0200

    ADD style to contacts header

--- public/src/screens/Contacts/index.jsx
+++ public/src/screens/Contacts/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import profile from '../../assets/images/profile';
+import { BsPlusCircleFill } from 'react-icons/bs';
 export default () => {
   return (
     <div className='contacts'>
-      <div className='contacts--header'>header </div>
-      <div className='contacts--search'>search</div>
-      <div className='contacts--body'>body</div>
+      <div className='contacts__header'>
+        <div>
+          <img src={profile} className='contact__img' />
+          <p className='title'> Chats </p>
+        </div>
+        <BsPlusCircleFill className='contacts__menu' />
+      </div>
     </div>
   );
 };
