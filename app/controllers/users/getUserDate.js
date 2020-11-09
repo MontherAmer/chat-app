bbdbb586013eb4865680e89b9179e44271a9076b
montheramer <monther.amer48@gmail.com>
2020-11-09 23:07:26 +0200

    CREATE function to get user date

---
+++ app/controllers/users/getUserDate.js
@@ -0,0 +1,22 @@
+const { User } = require('../../models');
+const { errorHandler, createJWT, userDataRes } = require('../../utils');
+
+exports.getUserData = async (req, res) => {
+  try {
+    let { _id } = req.params;
+
+    // * find user with req.email
+    let user = await User.findById(_id);
+    if (!user) return errorHandler('no user', res);
+
+    // * create token
+    let token = createJWT({ _id, email: user.email });
+
+    let data = await userDataRes(_id);
+    res.cookie('ChAt_ApP_CoOkIeS', token);
+    console.log('BEFORE RESPONSE ', { ...data, token });
+    return res.send({ success: true, status: 200, data: { ...data, token } });
+  } catch (err) {
+    return errorHandler(err, res);
+  }
+};
