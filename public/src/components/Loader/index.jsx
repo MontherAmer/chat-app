78136dac954d49455a401208e9f492f865209ed9
montheramer <monther.amer48@gmail.com>
2020-11-11 00:45:47 +0200

    CREATE loader component

---
+++ public/src/components/Loader/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className='loader'>
+      <div className='loading'>
+        <div className='bounceball'></div>
+        <div className='loader__text'>NOW LOADING</div>
+      </div>
+    </div>
+  );
+};
