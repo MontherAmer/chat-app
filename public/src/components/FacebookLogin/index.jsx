fd58c5e1b50dd75f449c58a99e237cf4b3102fc5
montheramer <monther.amer48@gmail.com>
2020-11-10 22:55:47 +0200

    CREATE facebook login component

---
+++ public/src/components/FacebookLogin/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { GrFacebookOption } from 'react-icons/gr';
+
+export default () => {
+  return (
+    <a href='api/auth/facebook' className='fb-login'>
+      <GrFacebookOption />
+      <p>Google</p>
+    </a>
+  );
+};
