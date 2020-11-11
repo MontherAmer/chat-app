aeaf414be04db3885f96473eec3489e8e818cc1b
montheramer <monther.amer48@gmail.com>
2020-11-11 09:50:29 +0200

    ADD facebook stratigy to passport middleware

--- app/middlewares/passport.js
+++ app/middlewares/passport.js
@@ -1,22 +1,46 @@
 const { User } = require('../models');
 const passport = require('passport');
-
+const jwt = require('jsonwebtoken');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
+const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/user/google/callback'
+      callbackURL: '/api/auth/google/callback'
     },
-    function (accessToken, refreshToken, profile, done) {
-      console.log('GOOGLE ', profile);
-      userProfile = profile;
-      return done(null, userProfile);
+    async function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
     }
   )
 );
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: '/api/auth/facebook/callback',
+      profileFields: ['email', 'name']
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
 
 exports.passport = passport;
