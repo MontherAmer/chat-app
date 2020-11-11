c77556a4455336d6a6af1fb135b30ed23a1906c5
montheramer <monther.amer48@gmail.com>
2020-11-11 09:49:05 +0200

    CREATE sign up controller

---
+++ app/controllers/auth/_signUp.js
@@ -0,0 +1,25 @@
+const { User } = require('../../models');
+const { errorHandler, createJWT, userDataRes } = require('../../utils');
+
+// *
+// * ─── SIGN UP NEW USER ───────────────────────────────────────────────────────────
+// *
+
+exports.store = async (req, res) => {
+  try {
+    let user = new User({ ...req.body });
+
+    // if !email or password return error
+    user = await user.save();
+
+    // * create token
+    let token = createJWT({ _id: user._id, email: user.email });
+
+    let data = await userDataRes(user._id);
+    res.cookie('ChAt_ApP_CoOkIeS', token);
+    return res.send({ success: true, status: 200, data: data });
+  } catch (err) {
+    console.log(err);
+    return errorHandler(err, res);
+  }
+};
