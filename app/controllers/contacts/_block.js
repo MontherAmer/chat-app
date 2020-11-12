a345efb81042e0217adbc0eba604f72c12799947
montheramer <monther.amer48@gmail.com>
2020-11-12 13:58:36 +0200

    update block contact

--- app/controllers/contacts/_block.js
+++ app/controllers/contacts/_block.js
@@ -1,12 +1,13 @@
-const { Contact } = require('../../models');
+const { Thread } = require('../../models');
 const { errorHandler, contactsList } = require('../../utils');
 
 exports.block = async (req, res) => {
   try {
     // * update blocked value from null to _id of the user
-    let contact = await Contact.findById(req.params._id);
-    contact.blocked = req._id;
-    await contact.save();
+    let thread = await Thread.findById(req.params._id);
+    thread.blocked = !thread.blocked;
+    await thread.save();
+
     let data = await contactsList(req._id);
     return res.send({ success: true, status: 200, data });
   } catch (err) {
