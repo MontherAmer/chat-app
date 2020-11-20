b2333a2e1e87faeb4b02ae7785317ec2352b4da7
montheramer <monther.amer48@gmail.com>
2020-11-20 17:25:49 +0200

    ADD search for chats

--- public/src/screens/Chats/index.jsx
+++ public/src/screens/Chats/index.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { updateScreen, updateActiveChat } from '../../store/actions';
 
+import { RiSearchLine } from 'react-icons/ri';
+
 import ChatItem from './Contact';
 
 export default () => {
+  const [state, setState] = useState({ search: '', contacts: [] });
   const { contacts } = useSelector(state => state.userState);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    setState({ ...state, contacts: contacts });
+  }, [contacts]);
+
+  const handleSearch = e => {
+    let value = e.target.value;
+    let newContacts = contacts.filter(contact => contact.name.includes(value));
+    setState({ ...state, search: e.target.value, contacts: newContacts });
+  };
 
   const handleChooseChat = e => {
     dispatch(updateScreen('Messages'));
@@ -19,8 +32,16 @@ export default () => {
       <div className='contacts_header'>
         <h4>Chats</h4>
       </div>
+
+      <div className='contact__input'>
+        <div className='contact__input__icon'>
+          <RiSearchLine />
+        </div>
+        <input type='text' autoComplete='off' placeholder='Search user' value={state.search || ''} onChange={handleSearch} />
+      </div>
+
       <div className='contact__thumbnails'>
-        {contacts.map(item => (
+        {state.contacts.map(item => (
           <ChatItem
             _id={item._id}
             image={item.image}
