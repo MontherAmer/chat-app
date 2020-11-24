c049c36598a4af8db520d2e82409c1875bebbb22
montheramer <monther.amer48@gmail.com>
2020-11-24 19:50:16 +0200

    create time function

--- public/src/utils/index.js
+++ public/src/utils/index.js
@@ -1,2 +1,3 @@
 export * from './constants';
 export * from './formValidation';
+export * from './time';
---
+++ public/src/utils/time.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+
+export const timeFormate = date => {
+  return moment(date).startOf('day').fromNow();
+};
