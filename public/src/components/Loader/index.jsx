fba56bbd74126fe96684638462dc2b09ecbebf07
montheramer <monther.amer48@gmail.com>
2021-01-05 17:17:53 +0200

    fix loader style

--- public/src/components/Loader/index.jsx
+++ public/src/components/Loader/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default ({ absolute }) => {
   return (
-    <div className={`${absolute}` ? 'loader-relative' : 'loader'}>
+    <div className={`${absolute ? 'loader-relative' : 'loader'}`}>
       <div className='loading'>
         <div className='bounceball'></div>
         <div className='loader__text'>NOW LOADING</div>
